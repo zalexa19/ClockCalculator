98e5996b1a475b9cdfbd4be2cb0d255d19cd32cd
Alex <zalexa19@gmail.com>
2018-05-27 09:46:37 -0700

    default date

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ var _HOURS_IN_DAY = 24;
 var _HOUR = 60;
 var _SECONDS = 60;
 var _MILISECONDS = 1000;
+var _MAX_MINUTES = 59;
+var _MIN_MINUTES = 0;
+var _MAX_HOUR = 23;
+var MIN_HOUR=_MIN_MINUTES;
 
 
 //Relevant to the page
@@ -68,21 +72,34 @@ function convert_ms_to_hr(msTime){
        extractedMinutes = (msTime /(_MILISECONDS*_SECONDS))%60;
        extractedDays = parseInt(extractedHour/24);
 
-      //console.log("result: "+ result + " \nHours: " + extractedHour + " \nMinutes: " + extractedMinutes);
       return result;
 }
 
-function validateHours(){
+
+function default_date () {
+
+    var todaysDate;
+
+    if (todayDD <10){
+    todayMM="0"+todayMM;
+    }
+
+    todaysDate = todayYYYY+"-"+"0"+todayMM+"-"+todayDD;
+
+    document.getElementById("fromDate").value=todaysDate;
+    document.getElementById("toDate").value=todaysDate;
 
 }
+
+
 
 function validateHours(element){
     var hour = element.value //document.getElementById("fromHour");
 
 
 
-    if (element.value>=24){
-        element.value="23";
+    if (element.value>_MAX_HOUR){
+        element.value=_MAX_HOUR;
     }
 
     if (element.value<0){
@@ -93,13 +110,19 @@ function validateHours(element){
 }
 
 
+
+
+/*
+    if the number inserted is invalid, it's change to the max minutes or min minutes
+    If the minutes are <10, 0 is added. unless 0 already exists
+ */
 function validate_minutes(element){
-    if (element.value <0){
-        element.value=0;
+    if (element.value <_MIN_MINUTES){
+        element.value=_MIN_MINUTES;
     }
 
-    if (element.value > 59){
-        element.value=59;
+    if (element.value > _MAX_MINUTES){
+        element.value=_MAX_MINUTES;
     }
 }
 
@@ -107,23 +130,13 @@ function enhance_number(element){
     var number = Number(element.value);
 
 
-    console.log(number);
-
-
-
-
     if (number < 10) {
-
         var num=number/10;
         console.log(num);
-
 
         if (parseInt(num)==0){
 
             element.value = '0'+number;
         }
-
-
     }
-
 }
